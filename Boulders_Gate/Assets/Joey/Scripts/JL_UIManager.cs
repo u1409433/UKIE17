e7cfbf337bcad0bed438250f92d38cb3f26613f7
u1409433 <u1409433@users.noreply.github.com>
2017-05-17 19:00:03 +0100

    Ktes

--- Boulders_Gate/Assets/Joey/Scripts/JL_UIManager.cs
+++ Boulders_Gate/Assets/Joey/Scripts/JL_UIManager.cs
@@ -8,12 +8,19 @@ public class JL_UIManager : MonoBehaviour
     public Text UI_ShotsLeft;
     public Slider UI_PowerSlider;
 
+    public Text UI_BricksLeft;
+    public Text UI_TotalBricks;
+
+    private int IN_BrickstoDestroy;
+
     private JL_LevelManager SC_LevelManager;
 
     // Use this for initialization
     void Start()
     {
         SC_LevelManager = GameObject.Find("LevelManager").GetComponent<JL_LevelManager>();
+        IN_BrickstoDestroy = SC_LevelManager.IN_BlocksLeft / 2;
+        UI_TotalBricks.text = IN_BrickstoDestroy.ToString();
     }
 
     // Update is called once per frame
@@ -21,5 +28,7 @@ public class JL_UIManager : MonoBehaviour
     {
         UI_ShotsLeft.text = SC_LevelManager.FL_ShotsLeft.ToString();
         UI_PowerSlider.value = SC_LevelManager.FL_Power;
+        int BricksLeft = IN_BrickstoDestroy * 2 - SC_LevelManager.IN_BlocksLeft;
+        UI_BricksLeft.text = BricksLeft.ToString();
     }
 }
