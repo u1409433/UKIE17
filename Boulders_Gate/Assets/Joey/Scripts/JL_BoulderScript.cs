94bfc52a40fc373bc680fbb08e0f7b7735f5b7e4
JohannesGames <j.brunn@hotmail.com>
2017-05-18 18:30:24 +0100

    beeppboop

--- Boulders_Gate/Assets/Joey/Scripts/JL_BoulderScript.cs
+++ Boulders_Gate/Assets/Joey/Scripts/JL_BoulderScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class JL_BoulderScript : MonoBehaviour
 {
+    public bool bl_prediction;
     Rigidbody RB;
     JL_LevelManager SC_LevelManager;
 
@@ -13,9 +14,10 @@ public class JL_BoulderScript : MonoBehaviour
         SC_LevelManager = GameObject.Find("LevelManager").GetComponent<JL_LevelManager>();
 
         RB = gameObject.GetComponent<Rigidbody>();
-
         RB.AddForce(transform.up * 600 * SC_LevelManager.FL_Power);
         RB.AddForce(-transform.forward * 75 * SC_LevelManager.FL_Power);
+        if (bl_prediction)
+            Destroy(gameObject, 0.2f);
     }
 
     // Update is called once per frame
@@ -28,7 +30,13 @@ public class JL_BoulderScript : MonoBehaviour
     {
         if (vCollided.transform.tag == "CollisionPlane")
         {
-            Invoke("Die", 3);
+            if (!bl_prediction)
+                Invoke("Die", 3);
+            else
+            {
+                transform.GetChild(0).parent = null;
+                Destroy(gameObject);
+            }
         }
     }
 
